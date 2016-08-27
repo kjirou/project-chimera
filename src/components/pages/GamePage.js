cd2f872a03f0ac29d7495fc11fcaf3ebb0cfb768
kjirou <kjirou.web@gmail.com>
2016-08-28 06:23:42 +0900

    Disable elastic scroll bounce

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+import { computeScrollTopToKeepOnInBounds } from '../../lib/utils';
 import PartBar from '../bars/PartBar';
 import SkillBar from '../bars/SkillBar';
 import Page from './Page';
 
 
 export default class GamePage extends Page {
+  _handleScroll(event) {
+    const nextScrollTop = computeScrollTopToKeepOnInBounds(this._skillsScrollerNode, this._skillsNode);
+    if (nextScrollTop !== null) {
+      this._skillsNode.scrollTop = nextScrollTop;
+    }
+  }
+
+  constructor() {
+    super();
+
+    this._skillsNode = null;
+    this._skillsScrollerNode = null;
+  }
+
+  componentDidMount() {
+    const node = ReactDOM.findDOMNode(this);
+    this._skillsNode = node.querySelector('.game-page__parts-and-skills');
+    this._skillsScrollerNode = this._skillsNode.querySelector('ul');
+  }
 
   render() {
-
     const _createCards = (count, className) => {
       return Array.from({ length: count }).map((notUsed, index) => {
         return <div key={ `card-${ index }` } className={ className }>
@@ -18,7 +38,10 @@ export default class GamePage extends Page {
     };
 
     return (
-      <div className="game-page">
+      <div
+        className="game-page"
+        onScroll={ this._handleScroll.bind(this) }
+      >
         <div className="game-page__header">
           Header
         </div>
@@ -33,20 +56,22 @@ export default class GamePage extends Page {
           <div className="next-button">Next</div>
           <div className="system-button">Sys</div>
         </div>
-        <ul className="game-page__parts-and-skills">
-          <li className="category-partition">Parts</li>
-          <PartBar />
-          <PartBar />
-          <PartBar />
-          <PartBar />
-          <PartBar />
-          <li className="category-partition">Skills</li>
-          <SkillBar />
-          <SkillBar />
-          <SkillBar />
-          <SkillBar />
-          <SkillBar />
-        </ul>
+        <div className="game-page__parts-and-skills">
+          <ul>
+            <li className="category-partition">Parts</li>
+            <PartBar />
+            <PartBar />
+            <PartBar />
+            <PartBar />
+            <PartBar />
+            <li className="category-partition">Skills</li>
+            <SkillBar />
+            <SkillBar />
+            <SkillBar />
+            <SkillBar />
+            <SkillBar />
+          </ul>
+        </div>
       </div>
     );
   }
